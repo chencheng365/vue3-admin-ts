36e702598e033863cb7378ee108fde2518450a76
陈程 <chencheng365@dingtalk.com>
2023-03-28 10:26:48 +0800

    fix: 🧩 自动化

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -98,7 +98,7 @@ export function validatenumber(number) {
 
 /**
  * 数字
- * @param {string} string-test-commit-aaaa-sadf-test
+ * @param {string} string-test-commit-aaaa-sadf-test-9999
  */
 export function isNumber(number: string) {
   const reg = /^(([^0][0-9]+|0)\.([0-9]+)$)|^(([^0][0-9]+|0)$)|^(([1-9]+)\.([0-9]+)$)|^(([1-9]+)$)/
